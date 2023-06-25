6b36d1a28ebf7b1c54c6976944d7bb16f00bd036
senghoho <qisstun@naver.com>
2023-06-25 23:16:58 +0900

    Feat: 학생복지 Welfare 모델 필드 추가

--- welfare/models.py
+++ welfare/models.py
@@ -6,16 +6,22 @@ from accounts.models import CustomUser
 class Welfare(models.Model):
     title = models.CharField(max_length=200)
     writer = models.ForeignKey(CustomUser, on_delete = models.CASCADE )
-    pub_date = models.DateTimeField()
+    start_time = models.TimeField(null = True) # 영업시간_시작
+    end_time = models.TimeField(null = True) # 영업시간_끝
+    address = models.CharField(max_length = 30) # 주소
+    start_date = models.DateTimeField(null=True, blank=True)
+    end_date = models.DateTimeField(null=True, blank=True)
+    category_univ = models.CharField(max_length=50, null = False, blank = True) # 게시판 선택
+    category_type = models.CharField(max_length=50, null = False, blank = True) # 카테고리
     body = models.TextField()
     image = models.ImageField(upload_to='welfare/')
     welfare_like = models.ManyToManyField(CustomUser, related_name='welfare_likes', blank = True)
     welfare_like_count = models.PositiveIntegerField(default = 0)
     
+
+
     def __str__(self):
         return self.title
     
     def summary(self):
         return self.body[:20]
-    
-    
